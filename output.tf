82357b383615a9d2f40273b66cfffa1fb69c274d
Rob Coward <rob@jive-videos.net>
2016-09-21 22:41:31 +0100

    Servernames output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "jumpbox_address" {
   value = "${openstack_compute_floatingip_v2.jumpbox_host_ip.address}"
 }
+
+output "server_names" {
+	value = ["${list(openstack_compute_instance_v2.jumpbox_host.name)}"]
+}
